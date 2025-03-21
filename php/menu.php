7c656695a8d4b1ed1125dbfae695e4c63cf3477d
sakanafurai <info@via.rossa.cc>
2025-03-21 21:36:15 +0900

    Move menu links into menu.php

---
+++ php/menu.php
@@ -0,0 +1,9 @@
+  <!-- メニューリンク -->
+    <a href="<?php echo Theme::siteUrl() ?>">ホーム</a>
+    <a href="<?php echo Theme::siteUrl() ?>about/">About</a>
+    <a href="<?php echo Theme::siteUrl() ?>chara-indices">キャラ別インデックス</a>
+    <a href="<?php echo Theme::siteUrl() ?>keyword-index">キーワード索引</a>
+    <a href="<?php echo Theme::siteUrl() ?>chara-data">キャラ基本データ</a>
+    <a href="<?php echo Theme::siteUrl() ?>arekore">あれこれ</a>
+    <a href="<?php echo Theme::siteUrl() ?>updates">更新履歴</a>
+    <a href="https://wavebox.me/wave/7namvh7yg7t9sg8l/">Wavebox</a>
--- php/sidebar.php
+++ php/sidebar.php
@@ -4,15 +4,9 @@
     <img src="<?php echo ($site->logo()); ?>" alt="<?php echo $site->title(); ?>">
     <?php endif ?>
   </figure>
+
   <nav class="sidebar-menu">
-    <a href="<?php echo Theme::siteUrl() ?>">ホーム</a>
-    <a href="<?php echo Theme::siteUrl() ?>about/">About</a>
-    <a href="<?php echo Theme::siteUrl() ?>chara-indices">キャラ別インデックス</a>
-    <a href="<?php echo Theme::siteUrl() ?>keyword-index">キーワード索引</a>
-    <a href="<?php echo Theme::siteUrl() ?>chara-data">キャラ基本データ</a>
-    <a href="<?php echo Theme::siteUrl() ?>arekore">あれこれ</a>
-    <a href="<?php echo Theme::siteUrl() ?>updates">更新履歴</a>
-    <a href="https://wavebox.me/wave/7namvh7yg7t9sg8l/">Wavebox</a>
+  <?php include(THEME_DIR_PHP.'menu.php'); ?>
   </nav>
   <p class="copyright"><span>since 2024-12-02</span></p>
   <!--<p class="copyright">Powered by <a href="https://bludit.com/" target="blank">Bludit</a></p>-->
